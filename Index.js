4dc01d4d34e438aeac041e3ca5c7c33d4a84fc31
ImranHossain5722 <imd572258@gmail.com>
2022-04-26 17:15:08 +0600

    this code my Car Service web site bacend coe with mongodb database

---
+++ Index.js
@@ -0,0 +1,80 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// dataBase connected
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4jfbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//database async function run 
+async function run (){
+    try{
+        await client.connect();
+
+        const serviceCollection = client.db('car').collection('service')
+        
+        app.get('/service',async (req,res)=>{
+
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const service = await cursor.toArray();
+            res.send(service)
+        });
+
+        // load a single service in ui 
+        app.get('/service/:id', async ( req,res) =>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        
+        app.post('/service',async(req,res)=>{
+            const newService = req.body
+            const result = await serviceCollection.insertOne(newService)
+            res.send(result);
+
+        })
+
+
+        app.delete('/service/:id', async (req,res)=>{
+            const id =req.params.id
+            const query = { _id: ObjectId(id)}
+
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result)
+
+        })
+
+
+
+    }
+    finally{
+        
+    }
+
+}
+run().catch(console.dir)
+
+app.get('/', (req,res) => {
+    
+    res.send('Running Server')
+
+})
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('Listening to port' ,port);
+})
